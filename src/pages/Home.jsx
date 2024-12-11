5a7765288e3bbc9bd5ba1b2a398f33e7ae2a6ef9
MuxN4 <stev3npy@gmail.com>
2024-12-11 17:45:34 +0300

    implement sound functionality  to Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
 import Loader from '../components/Loader'
@@ -7,9 +7,26 @@ import Sky from '../models/Sky'
 import Plane from '../models/Plane'
 import HomeInfo from '../components/HomeInfo'
 
+import water from '../assets/water.mp3'
+import { soundon, soundoff } from '../assets/icons'
+
 const Home = () => {
+  const audioRef = useRef(new Audio(water));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
+
   const [ isRotating, setIsRotating ] = useState(false);
   const [ currentStage, setCurrentStage ] = useState(1)
+  const [ isPlayingMusic, setIsPlayingMusic ] = useState(false)
+
+  useEffect(() =>{
+    if(isPlayingMusic){
+      audioRef.current.play();
+    }
+    return () => {
+      audioRef.current.pause();
+    }
+  },[isPlayingMusic])
 
   const adjustMillForScreenSize = () => {
     let screenScale = null;
@@ -79,6 +96,14 @@ const Home = () => {
           />
         </Suspense>
       </Canvas>
+      <div className='absolute bottom-2 left-2'>
+        <img 
+          src={!isPlayingMusic ? soundoff : soundon} 
+          alt='sound' 
+          className='w-10 h-10 cursor-pointer object-contain'
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+        />
+      </div>
     </section>
   )
 }
