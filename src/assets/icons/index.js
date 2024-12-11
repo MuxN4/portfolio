767a5ca73637d7c4de1af476fdeff172385b9625
MuxN4 <stev3npy@gmail.com>
2024-12-11 18:08:43 +0300

    assets

---
+++ src/assets/icons/index.js
@@ -0,0 +1,69 @@
+import css from './css.svg'
+import express from './express.svg'
+import git from './git.svg'
+import github from './github.svg'
+import githubActions from './githubactions.svg'
+import html from './html.svg'
+import javascript from './javascript.svg'
+import mongodb from './mongodb.svg'
+import postgresSQL from './postgresSQL.svg'
+import motion from './motion.svg'
+import nextjs from './nextjs.svg'
+import nodejs from './nodejs.svg'
+import react from './react.svg'
+import sqlalchemy from './sqlalchemy.svg'
+import go from './go.svg'
+import python from './python.svg'
+import django from './django.svg'
+import postman from './postman.svg'
+import flask from './flask.svg'
+import tailwindcss from './tailwindcss.svg'
+import typescript from './typescript.svg'
+import linkedin from './linkedin.svg'
+import twitter from './twitter.svg'
+import car from './car.svg'
+import estate from './estate.svg'
+import camping from './camping.svg'
+import snapgram from './snapgram.svg'
+import summiz from './summiz.svg'
+import threads from './threads.svg'
+import arrow from './arrow.svg'
+import contact from './contact.svg'
+import soundon from './soundon.png'
+import soundoff from './soundoff.png'
+
+export {
+    css,
+    express,
+    git,
+    github,
+    githubActions,
+    html,
+    javascript,
+    mongodb,
+    postgresSQL,
+    motion,
+    go, 
+    python, 
+    django,
+    postman,
+    flask,
+    nextjs,
+    nodejs,
+    react,
+    sqlalchemy,
+    tailwindcss,
+    typescript,
+    linkedin,
+    twitter,
+    car,
+    estate,
+    camping,
+    snapgram,
+    summiz,
+    threads,
+    arrow,
+    contact,
+    soundon,
+    soundoff
+}
