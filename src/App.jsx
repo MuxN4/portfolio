19cd1301c5c4ef516aa470c6c31d17ef36836d21
MuxN4 <stev3npy@gmail.com>
2024-12-11 17:44:09 +0300

    conditionally render Footer in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
-import React from 'react'
-import Navbar from './components/Navbar'
-import {Home, About, Projects, Contact } from './pages'
+import { Route, BrowserRouter as Router, Routes, useLocation } from 'react-router-dom';
+import React from 'react';
+import Navbar from './components/Navbar';
+import { Home, About, Projects, Contact } from './pages';
+import Footer from './components/Footer';
 
 const App = () => {
+  const FooterWrapper = () => {
+    const location = useLocation();
+
+    // Only show the Footer if the current path is not '/'
+    return location.pathname !== '/' ? <Footer /> : null;
+  };
+
   return (
     <main className='bg-slate-300/20'>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/about' element={<About />}/>
-          <Route path='/projects' element={<Projects />}/>
-          <Route path='/contact' element={<Contact />}/>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
+        <FooterWrapper />
       </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
