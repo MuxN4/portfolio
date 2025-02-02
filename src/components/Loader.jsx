7dfb552f986cad4650b73b4f158efe47dfe4da0f
MuxN4 <stev3npy@gmail.com>
2025-02-02 11:24:04 +0300

    feat: loader with 3D animated boxes

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import { Html } from '@react-three/drei'
+import React from 'react';
+import { Html } from '@react-three/drei';
+import './Loader.css';
 
 const Loader = () => {
   return (
     <Html>
-      <div className='flex justify-center items-center'>
-        <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
+      <div className="boxes">
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="box">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </Html>
-    
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
